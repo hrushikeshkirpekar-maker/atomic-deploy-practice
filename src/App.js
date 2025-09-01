20a4fd24802764c3d8f41b7b82b6dfcfa198d4ca
Hrushikesh Kirpekar <hrushikesh.kirpekar@diginnovators.com>
2025-09-01 15:20:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>DigiInnovators</h1>
+        <p>Welcome to DigiInnovators </p>
+        <p>Deployment successful with GitHub + Atomic Deployment </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
